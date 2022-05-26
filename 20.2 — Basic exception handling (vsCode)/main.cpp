f38c797862b82a6f1d622ecb970417423e52952e
Mateusz <oureyelet@gmail.com>
2022-05-26 20:41:05 +0100

    20.2 — Basic exception handling (vsCode)

--- 20.2 — Basic exception handling (vsCode)/main.cpp
+++ 20.2 — Basic exception handling (vsCode)/main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <string_view>
+#include <locale>
+#include <cmath> // for sqrt() function
+
+std::string_view name(std::string name)
+{
+    try
+    {
+        if(!isupper(name[0]))
+        {
+            throw "Your name stared lowercase\n";
+        }
+
+        std::cout << "Your name is: " << name << '\n';
+    }
+    catch(const char* ex) // note: no variable name since we don't use it in the catch block below
+    {
+            std::cout << ex << "I hanged your name to uppercase." << '\n';
+            name[0] = std::toupper(name[0]);
+            std::cout << name << '\n';
+
+    }
+    
+
+    return name;
+}
+
+
 
 int main()
 {
@@ -18,11 +46,47 @@ int main()
     }*/
 
     //Handling exceptions:   START FROM HERE <<<---- again...
-    catch(int x)
+  /*  catch(int x)
     {
         // Handle an exception of type int here
         std::cerr << "We caught an int exception with value" << x << '\n';
+    }*/
+
+    //Putting throw, try, and catch together:
+    //std::cout << dividing(3, 8) << '\n';
+    //std::cout << dividing(3, 0) << '\n';
+
+    std::string yourname{};
+    std::cout << "Enter your name:";
+    std::cin >> yourname;
+    name(yourname);
+
+    //Recapping exception handling:
+
+    //Exceptions are handled immediately:
+
+    //A more realistic example:
+    std::cout << "Enter a number: ";
+    double x{};
+    std::cin >> x;
+
+    try// Look for exceptions that occur within try block and route to attached catch block(s)
+    {
+        // If the user entered a negative number, this is an error condition
+        if(x < 0.0)
+            throw "Can not take sqrt of negative number\n"; // throw exception of type const char*
+
+        // Otherwise, print the answer
+        std::cout << "The sqrt of " << x << " is " << std::sqrt(x) << '\n';
+
     }
+    catch(const char* exeption) // catch exceptions of type const char*
+    {
+        std::cerr << "Error: " << exeption << '\n';
+    }
+
+    //What catch blocks typically do:
+    
 
     return 0;
 }
