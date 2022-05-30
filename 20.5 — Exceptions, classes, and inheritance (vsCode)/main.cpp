31cbdeadb4bb3ce524f2a57f394850d4f73b2636
Mateusz <oureyelet@gmail.com>
2022-05-30 20:04:09 +0100

    try, throw, catch

--- 20.5 — Exceptions, classes, and inheritance (vsCode)/main.cpp
+++ 20.5 — Exceptions, classes, and inheritance (vsCode)/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cassert>
+#include <string>
+#include <string_view>
 
 template<typename T>
 class Array
@@ -11,7 +13,7 @@ private:
 public:
     Array(T length)
     {
-        assert(length > 0);
+        //assert(length > 0);
         m_data = new T[length]{};
         m_length = length;
     }
@@ -29,30 +31,155 @@ public:
 template<typename T>
 T& Array<T>::operator[](const T index)
 {
-    assert(index > 0 && index < getLength());
+   // assert(index >= 0 && index <= getLength()); // how we read this: if index will be greather then 0 is good ! 
+
+    if(index < 0 || index > getLength())
+        throw index;
+
     return m_data[index];
 }
 
+class Member
+{
+public:
+    Member()
+    {
+        std::cerr << "Member allocated some resources\n";
+    }
 
+    ~Member()
+    {
+        std::cerr << "Member cleaned up\n";
+    }
+};
+
+class A
+{
+private:
+    int m_x{};
+    Member member;
+
+public:
+    A(int x)
+        :m_x{ x }
+    {
+        if(x <= 0)
+            throw -1;
+    }
+
+    ~A()
+    {
+        std::cerr << "~A\n";// should not be called
+    }
+};
+
+class ArrayException
+{
+private:
+    std::string m_error{};
+
+public:
+    ArrayException(std::string error)
+        : m_error{ error }
+    {
+    }
+
+    std::string_view getError() const { return m_error; }
+
+};
+
+
+
+
+template<typename T>
+class IntArray
+{
+private:
+    T m_data[3]{};// assume array is length 3 for simplicity
+
+public:
+    IntArray() = default;
+
+    T getLength() const { return 3; }
+
+    T& operator[](const T index)
+    {
+        if(index < 0 || index >= getLength())
+            throw ArrayException{ "Invalid index" };
+        return m_data[index];
+    }
+
+};
+
+class Base
+{
+public:
+    Base(){}
+};
+
+class Derived: public Base
+{
+public:
+    Derived(){}
+};
 
 int main()
 {
     //Exceptions and member functions:
-    Array intArray{ 120 };
+    Array my_intArray{ 120 };
 
-    for(int count{ 0 }; count < 120; ++count)
+    for(int count{ 0 }; count < 121; ++count)
     {
-        intArray[count] = count;
+        my_intArray[count] = count;
     }
-/*
+
     for(int count{ 120 }; count > 0; --count)
     {
-        std::cout << intArray[count] << ' ';
+        std::cout << my_intArray[count] << ' ';
     }
     std::cout << std::endl;
-*/
 
-//    check our assert why not working ?!?!?!
+    //When constructors fail:
+    
+    try
+    {
+        A a{ 0 };
+    }
+    catch(int)
+    {
+        std::cerr << "Oops\n" << '\n';
+    }
+    
+    //Exception classes:
+    IntArray<int> array;
+
+    try
+    {
+        int value{ array[5] }; // out of range subscript
+    }
+    catch(const ArrayException& exeption)
+    {
+        std::cerr << "An array exception occurred (" << exeption.getError() << ")\n";
+    }
+    
+    //Exceptions and inheritance:
+    try
+    {
+        throw Derived();
+    }
+    catch(const Derived& derived)
+    {
+        std::cerr << "caught derived" << '\n';
+    }
+    catch(const Base& base)
+    {
+        std::cerr << "caught derived" << '\n';
+    }
+    
+    //std::exception:
+    
+
+
 
     return 0;
 }
